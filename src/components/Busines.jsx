8d640b8b9a936a9e498d26839ca3a14c8613dcb1
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-09-13 11:58:15 +0500

    commit

--- src/components/Busines.jsx
+++ src/components/Busines.jsx
@@ -8,8 +8,10 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/scrollbar";
 import { FreeMode, Scrollbar, Mousewheel } from "swiper";
+import { useTranslation } from "react-i18next";
 
 function Busines() {
+  const [t, i18next] = useTranslation();
   return (
     <div className={styles.busines}>
       <Swiper
@@ -25,9 +27,12 @@ function Busines() {
         {data?.map((evt) => (
           <SwiperSlide className={styles.businesItem}>
             <div key={evt.id}>
-              <img src={evt.img} alt={evt.name} />
-              <h2 className="businesName">{evt.title}</h2>
-              <p className="businesText">{evt.text}</p>
+              <img src={evt.img} alt="" />
+              <h2 className="businesName">
+                {" "}
+                {evt[`title_${i18next.language}`]}
+              </h2>
+              <p className="businesText">{evt[`text_${i18next.language}`]}</p>
             </div>
           </SwiperSlide>
         ))}
--- src/components/data.js
+++ src/components/data.js
@@ -10,90 +10,186 @@ import BusinesNine from "../assets/group9.png";
 import BusinesTen from "../assets/group10.png";
 import BusinesEleven from "../assets/group11.png";
 import BusinesTwelf from "../assets/group12.png";
+import BusinesThirty from "../assets/group13.png";
 
 export const data = [
   {
     id: 1,
-    title: "вид на город",
-    text: "Благодаря продуманным видам планировок, разместить всё необходимое можно будет таким образом, чтобы лицезреть виды столицы было комфортно и приятно.",
+    title_ru: "вид на город",
+    title_en: "city view",
+    title_uz: "shahar ko'rinishi",
+    text_ru:
+      "Благодаря продуманным видам планировок, разместить всё необходимое можно будет таким образом, чтобы лицезреть виды столицы было комфортно и приятно.",
+    text_uz:
+      "O'ylangan tartib turlari tufayli sizga kerak bo'lgan hamma narsa poytaxt manzaralarini tomosha qilish uchun qulay va yoqimli bo'ladigan tarzda joylashtirilishi mumkin.",
+    text_en:
+      "Thanks to thoughtful types of layouts, everything you need can be placed in such a way that it is comfortable and pleasant to contemplate the views of the capital.",
     img: BusinesOne,
   },
   {
     id: 2,
-    title: "внешний вид",
-    text: "Общий вид комплекса будет аккуратным благодаря тому, что все коммуникации будут скрыты. ",
+    title_ru: "внешний вид",
+    title_uz: "tashqi ko'rinish",
+    title_en: "appearance",
+    text_ru:
+      "Общий вид комплекса будет аккуратным благодаря тому, что все коммуникации будут скрыты. ",
+    text_en:
+      "The overall appearance of the complex will be neat due to the fact that all communications will be hidden.",
+    text_uz:
+      "Kompleksning umumiy ko'rinishi toza bo'ladi, chunki barcha kommunikatsiyalar yashirin bo'ladi.",
     img: BusinesTwo,
   },
   {
     id: 3,
-    title: "бизнес центр",
-    text: "Для жителей Modera Towers она может заиграть по-новому, ведь теперь для того, чтобы оказаться в офисе, нет необходимости даже выходить на улицу.",
+    title_ru: "бизнес центр",
+    title_uz: "Biznes Markazi",
+    title_en: "business center",
+    text_ru:
+      "Для жителей Modera Towers она может заиграть по-новому, ведь теперь для того, чтобы оказаться в офисе, нет необходимости даже выходить на улицу.",
+    text_en:
+      "For residents of Modera Towers, it can play in a new way, because now in order to be in the office, there is no need to even go outside.",
+    text_uz:
+      "Modera Towers aholisi uchun u yangi usulda o'ynashi mumkin, chunki endi ofisda bo'lish uchun hatto tashqariga chiqishning hojati yo'q.",
     img: BusinesThree,
   },
   {
     id: 4,
-    title: "ДВОР",
-    text: "Шестой этаж комплекса отведён под двор на стилобате высотой под 30 метров и будет нести несколько функций.",
+    title_ru: "ДВОР",
+    title_en: "YARD",
+    title_uz: "HAVLI",
+    text_ru:
+      "Шестой этаж комплекса отведён под двор на стилобате высотой под 30 метров и будет нести несколько функций.",
+    text_en:
+      "The sixth floor of the complex is dedicated to a courtyard on a stylobate 30 meters high and will serve several functions.",
+    text_uz:
+      "Majmuaning oltinchi qavati balandligi 30 metr bo‘lgan stilobat ustidagi hovliga mo‘ljallangan bo‘lib, u bir nechta vazifalarni bajaradi.",
     img: BusinesFour,
   },
   {
     id: 5,
-    title: "коммерческие зоны",
-    text: "Беспроигрышным решением для бизнеса станет приобретение коммерческой зоны в Modera Towers. ",
-    img: BusinesSix,
+    title_ru: "коммерческие зоны",
+    title_en: "commercial zones",
+    title_uz: "savdo zonalari",
+    text_ru:
+      "Беспроигрышным решением для бизнеса станет приобретение коммерческой зоны в Modera Towers. ",
+    text_en:
+      "A win-win solution for business would be the acquisition of a commercial zone in Modera Towers.",
+    text_uz:
+      "Biznes uchun g'alaba qozonish yechimi Modera Towersda tijorat zonasini sotib olish bo'ladi.",
+    img: BusinesFive,
   },
   {
     id: 6,
-    title: "лаунж-зона",
-    text: "Также, для удобства гостей, там же будет расположен фитобар и летняя кухня.",
-    img: BusinesSeven,
+    title_ru: "лаунж-зона",
+    title_uz: "dam olish maydoni",
+    title_en: "lounge area",
+    text_ru:
+      "Также, для удобства гостей, там же будет расположен фитобар и летняя кухня.",
+    text_en:
+      "Also, for the convenience of guests, there will be a herbal bar and a summer kitchen there.",
+    text_uz:
+      "Shuningdek, mehmonlarga qulaylik yaratish maqsadida u yerda o‘simlik bari va yozgi oshxona bo‘ladi.",
+    img: BusinesSix,
   },
   {
     id: 7,
-    title: "зона с шезлонгами",
-    text: "Вы и ваши гости сможете найти место как для спокойного отдыха в тени, так и для принятия солнечных ванн.",
-    img: BusinesEight,
+    title_ru: "зона с шезлонгами",
+    title_uz: "quyosh kreslolari bo'lgan maydon",
+    title_en: "area with sun loungers",
+    text_ru:
+      "Вы и ваши гости сможете найти место как для спокойного отдыха в тени, так и для принятия солнечных ванн.",
+    text_en:
+      "You and your guests will be able to find a place for both relaxing in the shade and sunbathing.",
+    text_uz:
+      "Siz va sizning mehmonlaringiz soyada dam olish va quyoshda cho'milish uchun joy topa olasiz.",
+    img: BusinesSeven,
   },
   {
     id: 8,
-    title: "ресторан",
-    text: "Для удобства жильцов и гостей на шестом этаже комплекса мы также предлагаем услуги ресторанов.",
-    img: BusinesNine,
+    title_ru: "ресторан",
+    title_uz: "restoran",
+    title_en: "restaurant",
+    text_ru:
+      "Для удобства жильцов и гостей на шестом этаже комплекса мы также предлагаем услуги ресторанов.",
+    text_en:
+      "For the convenience of residents and guests, we also offer restaurant services on the sixth floor of the complex.",
+    text_uz:
+      "Aholi va mehmonlarga qulaylik yaratish maqsadida majmuaning oltinchi qavatida restoran xizmatlarini ham taklif etamiz.",
+    img: BusinesEight,
   },
   {
     id: 9,
-    title: "детская площадка",
-    text: "Комфортабельное жильё Modera Towers отлично подойдёт как для молодёжи, так и для семей с детьми.",
-    img: BusinesTen,
+    title_ru: "детская площадка",
+    title_en: "playground",
+    title_uz: "o'yin maydonchasi",
+    text_ru:
+      "Комфортабельное жильё Modera Towers отлично подойдёт как для молодёжи, так и для семей с детьми.",
+    text_en:
+      "Comfortable accommodation at Modera Towers is ideal for both young people and families with children.",
+    text_uz:
+      "Modera Towersdagi qulay turar joy ham yoshlar, ham bolali oilalar uchun ideal.",
+    img: BusinesNine,
   },
   {
     id: 10,
-    title: "ПАРКИНГ",
-    text: "Modera Towers также предлагает владельцам бизнеса и жильцам 242 места с круглосуточной системой наблюдения.",
-    img: BusinesEleven,
+    title_ru: "ПАРКИНГ",
+    title_en: "PARKING",
+    title_uz: "Avtoturargoh",
+    text_ru:
+      "Modera Towers также предлагает владельцам бизнеса и жильцам 242 места с круглосуточной системой наблюдения.",
+    text_en:
+      "Modera Towers also offers business owners and residents 242 beds with 24-hour surveillance.",
+    text_uz:
+      "Modera Towers shuningdek, biznes egalari va aholisiga 24 soatlik kuzatuvga ega 242 ta yotoqni taklif etadi.",
+    img: BusinesTen,
   },
   {
     id: 11,
-    title: "спортивный клуб",
-    text: "Одним из самых важных преимуществ является то, что прямо в комплексе Modera Towers расположен современный спортивный клуб.",
-    img: BusinesTwelf,
+    title_ru: "спортивный клуб",
+    title_en: "sport Club",
+    title_uz: "sport klubi",
+    text_ru:
+      "Одним из самых важных преимуществ является то, что прямо в комплексе Modera Towers расположен современный спортивный клуб.",
+    text_en:
+      "One of the most important advantages is that a modern sports club is located right in the Modera Towers complex.",
+    text_uz:
+      "Eng muhim afzalliklaridan biri shundaki, zamonaviy sport klubi aynan Modera Towers majmuasida joylashgan.",
+    img: BusinesEleven,
   },
   {
     id: 12,
-    title: "спа-комплекс",
-    text: "Комплекс предназначен для людей, которые заботятся о своём здоровье и берегут драгоценное время.",
-    img: BusinesOne,
+    title_ru: "спа-комплекс",
+    title_uz: "kurort majmuasi",
+    title_en: "spa complex",
+    text_ru:
+      "Комплекс предназначен для людей, которые заботятся о своём здоровье и берегут драгоценное время.",
+    text_en:
+      "The complex is intended for people who care about their health and save precious time.",
+    text_uz:
+      "Majmua o'z salomatligi haqida qayg'uradigan va qimmatli vaqtni tejaydigan odamlar uchun mo'ljallangan.",
+    img: BusinesTwelf,
   },
   {
     id: 13,
-    title: "открытый бассейн",
-    text: "Открытый и закрытый бассейны будут оснащены современной системой очистки воды и зоной с шезлонгами",
-    img: BusinesOne,
+    title_ru: "открытый бассейн",
+    title_uz: "ochiq basseyn",
+    title_en: "open pool",
+    text_ru:
+      "Открытый и закрытый бассейны будут оснащены современной системой очистки воды и зоной с шезлонгами",
+    text_en:
+      "The outdoor and indoor pools will be equipped with a modern water purification system and an area with sun loungers",
+    text_uz:
+      "Ochiq va yopiq basseynlar zamonaviy suvni tozalash tizimi va quyosh uchun stullar bilan jihozlangan maydon bilan jihozlanadi",
+    img: BusinesThirty,
   },
   {
     id: 14,
-    title: "крытый бассейн",
-    text: "Modera Towers предлагает крытый бассейн на шестом этаже.",
+    title_ru: "крытый бассейн",
+    title_uz: "yopiq basseyn",
+    title_en: "indoor pool",
+    text_ru: "Modera Towers предлагает крытый бассейн на шестом этаже.",
+    text_uz: "Modera Towers oltinchi qavatda yopiq basseynni taklif etadi.",
+    text_en: "Modera Towers offers an indoor pool on the sixth floor.",
     img: BusinesFive,
   },
 ];
