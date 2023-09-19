d63d7d0902479e4787327c35b0c6a54da760405f
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-09-19 20:42:25 +0500

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -97,6 +97,56 @@ function Contact() {
               <img src={Location1} alt="Phone" className="modera-icon" />
             </a>
           </div>
+          <div className="contactList">
+            <a href="tel:+998940007777" className="moderaLink">
+              <img src={Phone} alt="Phone" className="modera-icon" />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61550525660613&mibextid=LQQJ4d"
+              className="moderaLink">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="59"
+                height="58"
+                viewBox="0 0 59 58"
+                fill="none">
+                <path
+                  d="M56.5306 16.8385V41.1862C56.5306 49.5905 49.7174 56.4036 41.3132 56.4036H16.9654C8.5611 56.4036 1.74805 49.5905 1.74805 41.1862V16.8385C1.74805 8.43415 8.5611 1.62109 16.9654 1.62109H41.3132C49.7174 1.62109 56.5306 8.43415 56.5306 16.8385Z"
+                  stroke="#8062D6"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M26.0958 56.404C26.0958 47.2736 26.0958 38.1432 26.0958 29.0128C26.0958 22.3552 27.6176 16.8389 38.2697 16.8389"
+                  stroke="#8062D6"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M20.0089 32.0562H26.0958H38.2697"
+                  stroke="#8062D6"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://instagram.com/moderatowers?igshid=MzRlODBiNWFlZA=="
+              className="moderaLink">
+              <img src={Instagram} alt="Phone" className="modera-icon" />
+            </a>
+            <a href="https://t.me/moderatowers" className="moderaLink">
+              <img src={Telegram} alt="Phone" className="modera-icon" />
+            </a>
+            <a
+              href="https://maps.app.goo.gl/81xuBj2eNQrri92d7"
+              className="moderaLink">
+              <img src={Location} alt="Phone" className="modera-icon" />
+            </a>
+          </div>
         </div>
       </div>
       <div className="footer">
